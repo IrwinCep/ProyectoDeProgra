abbdb9c7aabd67a07856ccb3249116c1f6b43280
IrwinCep <142370646+IrwinCep@users.noreply.github.com>
2023-11-23 01:36:04 -0600

    coin

--- Plataformero/Assets/Scripts/TesoroMoneda.cs
+++ Plataformero/Assets/Scripts/TesoroMoneda.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class TesoroMoneda : MonoBehaviour
 {
+    private Animator miAnimador;
+    public int vMoneda = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        miAnimador = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print(name + "hizo colisión con "
+            + other.gameObject.name);
+
+        GameObject otro = other.gameObject;
+        if (otro.tag == "Player")
+        {
+            Personaje elPerso = otro.GetComponent<Personaje>();
+            elPerso.score = elPerso.score + vMoneda;
+            miAnimador.SetTrigger("Desaparece");
+            GetComponent<Collider2D>().enabled = false;
+            Destroy(gameObject, 1.5f);
+        }
     }
 }
